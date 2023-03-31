192f42e4ef4cd72e05bd8cb7164787aa526a6e7b
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-03-31 17:02:47 -0300

    Configuração básica da view e do layout.

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.orgs
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Pipoca", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Pipoca", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
     }
 
 }
