afbbf936c7747ee944655a51de93338828a59c7e
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-04-04 15:40:43 -0300

    Implementado o FloatingActionButton e utilização do Intent para ir da main activity para a activity do formulário.,

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -45,8 +48,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 )
             )
         )
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton7)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
 //        Toast.makeText(this, "Pipoca", Toast.LENGTH_SHORT).show()
