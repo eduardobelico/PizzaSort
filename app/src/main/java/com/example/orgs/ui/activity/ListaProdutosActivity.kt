7b7933e4c36bde5eee18a2a26a9ebce5165d7c2e
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-04-06 15:16:28 -0300

    Refatorando a activity principal e o adapter com as funções de configuração do Recycler View e do Fab e fun de atualizar a lista mutavel no adapter.

---
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,45 @@
+package com.example.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
+
+    }
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+
+    }
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton7)
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = adapter
+    }
+}
+
+
