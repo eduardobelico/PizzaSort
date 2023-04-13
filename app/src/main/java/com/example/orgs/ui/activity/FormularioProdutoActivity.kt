050e67e934249a0225042136e6bd9c6cf7bdab64
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-04-13 13:37:56 -0300

    Configuracao da permissao para acesso a Internet no Manifestto; Coil adicionado como uma dependência; Vinculando imagem ao produto por meio de url na activity do formulario e no adapter.

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -3,9 +3,11 @@ package com.example.orgs.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
+import com.example.orgs.databinding.FormularioImagemBinding
 import com.example.orgs.model.Produto
 import java.math.BigDecimal
 
@@ -13,16 +15,28 @@ class FormularioProdutoActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityFormularioProdutoBinding.inflate(layoutInflater)
+//        FormularioImagemBinding.inflate(layoutInflater)
     }
+
+    private var url: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         configuraBotaoSalvar()
         setContentView(binding.root)
         binding.activityFormularioProdutoImagem.setOnClickListener() {
+            val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
+            bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
+                url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+                bindingFormularioImagem.formularioImagemImageview.load(url)
+            }
+
             AlertDialog.Builder(this)
-                .setView(R.layout.formulario_imagem)
-                .setPositiveButton("Confirmar") { _, _ -> }
+                .setView(bindingFormularioImagem.root)
+                .setPositiveButton("Confirmar") { _, _ ->
+                    val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+                    binding.activityFormularioProdutoImagem.load(url)
+                }
                 .setNegativeButton("Cancelar") { _, _ -> }
                 .show()
         }
@@ -55,7 +69,8 @@ class FormularioProdutoActivity : AppCompatActivity() {
         return Produto(
             nome = nome,
             descricao = descricao,
-            valor = valor
+            valor = valor,
+            imagem = url
         )
     }
 }
--- app/src/main/java/com/example/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/example/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -3,6 +3,7 @@ package com.example.orgs.ui.recyclerview.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.orgs.databinding.ProdutoItemBinding
 import com.example.orgs.extensions.formatToBrazilianCurrency
 import com.example.orgs.model.Produto
@@ -18,6 +19,7 @@ class ListaProdutosAdapter : RecyclerView.Adapter<ListaProdutosAdapter.ProdutoVi
                 produtoItemNome.text = produto.nome
                 produtoItemDescricao.text = produto.descricao
                 produtoItemValor.text = produto.valor.formatToBrazilianCurrency()
+                imageView.load(produto.imagem)
             }
         }
     }
