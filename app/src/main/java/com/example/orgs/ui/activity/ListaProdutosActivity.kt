460855ce47dbea7c5d3816a7025108278bf3128b
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-04-27 17:12:58 -0300

    Implementando as dependencias do lifeCyple. Refatorando o código para que a thread seja iniciada pelo lifecycleScoppe e encerrada sem o onDestroy. Uso do Main Safe com uma suspend function pora buscar os produtos.

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -2,18 +2,20 @@ package com.example.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.orgs.R
 import com.example.orgs.database.AppDatabase
 import com.example.orgs.databinding.ActivityListaProdutosBinding
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
-import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ListaProdutosActivity : AppCompatActivity() {
 
@@ -24,7 +26,10 @@ class ListaProdutosActivity : AppCompatActivity() {
     private val produtoDao by lazy {
         AppDatabase.instancia(this).produtoDao()
     }
-    private val job = Job()
+    private val dao by lazy {
+        val db = AppDatabase.instancia(this)
+        db.produtoDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,92 +38,73 @@ class ListaProdutosActivity : AppCompatActivity() {
         configuraFab()
     }
 
+
     override fun onResume() {
         super.onResume()
-        val db = AppDatabase.instancia(this)
-        val produtoDao = db.produtoDao()
-        val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
-            Toast.makeText(
-                this@ListaProdutosActivity,
-                "Ocorreu um problemão",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        val scope = MainScope()
-       scope.launch(job) {
-            repeat(1000) {
-                Log.i("MEW", "onResume: coroutine está em execução $it")
-                delay(1000)
-            }
-        }
-        scope.launch(handler) {
-            MainScope().launch(handler) {
-                throw Exception("hello")
-            }
-            throw IllegalArgumentException("oi")
-            val produtos = withContext(IO) {
-                produtoDao.buscaTodos()
-            }
+        lifecycleScope.launch {
+            val produtos = buscaTodosProdutos()
             adapter.setData(produtos)
         }
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        job.cancel()
+    // Conceito de Main Safe.
+    // Evitar que o app seja quebrado, logo usando o escopo que terá o seu vínculo de Dispatcher na Main Thread.
+    // pré-execução - apresentar um componente que indica o carregamento.
+    // pós-execução - pegar o resultado e fazer a atualização da main thread.
+    // A partir de uma suspend function se resolve a operação assíncerona internamente.
+    private suspend fun buscaTodosProdutos() = withContext(IO) {
+        dao.buscaTodos()
     }
 
-        private fun configuraRecyclerView() {
-            val recyclerView = binding.activityListaProdutosRecyclerView
-            recyclerView.adapter = adapter
-            vaiParaDetalhesProduto()
-            adapter.clicarEmEditar
-            adapter.clicarEmRemover
-        }
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaProdutosRecyclerView
+        recyclerView.adapter = adapter
+        vaiParaDetalhesProduto()
+        adapter.clicarEmEditar
+        adapter.clicarEmRemover
+    }
 
-        private fun configuraFab() {
-            val fab = binding.activityListaProdutosFab
-            fab.setOnClickListener {
-                vaiParaFormularioProduto()
+    private fun configuraFab() {
+        val fab = binding.activityListaProdutosFab
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun vaiParaDetalhesProduto() {
+        adapter.clicarNoProduto = {
+            val intent = Intent(this, DetalhesProdutoActivity::class.java).apply {
+                putExtra(CHAVE_PRODUTO_ID, it.id)
             }
-        }
-
-        private fun vaiParaFormularioProduto() {
-            val intent = Intent(this, FormularioProdutoActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        private fun vaiParaDetalhesProduto() {
-            adapter.clicarNoProduto = {
-                val intent = Intent(this, DetalhesProdutoActivity::class.java).apply {
-                    putExtra(CHAVE_PRODUTO_ID, it.id)
-                }
-                startActivity(intent)
-            }
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_ordenar_produtos, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            menuInflater.inflate(R.menu.menu_ordenar_produtos, menu)
-            return super.onCreateOptionsMenu(menu)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val produtosOrdenados: List<Produto>? = when (item.itemId) {
+            R.id.menu_ordenar_produtos_nome_asc -> produtoDao.ordenarPorNomeAsc()
+            R.id.menu_ordenar_produtos_nome_desc -> produtoDao.ordenarPorNomeDesc()
+            R.id.menu_ordenar_produtos_descricao_asc -> produtoDao.ordenarPorDescricaoAsc()
+            R.id.menu_ordenar_produtos_descricao_desc -> produtoDao.ordenarPorDescricaoDesc()
+            R.id.menu_ordenar_produtos_valor_asc -> produtoDao.ordenarPorValorAsc()
+            R.id.menu_ordenar_produtos_valor_desc -> produtoDao.ordenarPorValorDesc()
+            R.id.menu_ordenar_produtos_sem_ordem -> produtoDao.buscaTodos()
+            else -> null
         }
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            val produtosOrdenados: List<Produto>? = when (item.itemId) {
-                R.id.menu_ordenar_produtos_nome_asc -> produtoDao.ordenarPorNomeAsc()
-                R.id.menu_ordenar_produtos_nome_desc -> produtoDao.ordenarPorNomeDesc()
-                R.id.menu_ordenar_produtos_descricao_asc -> produtoDao.ordenarPorDescricaoAsc()
-                R.id.menu_ordenar_produtos_descricao_desc -> produtoDao.ordenarPorDescricaoDesc()
-                R.id.menu_ordenar_produtos_valor_asc -> produtoDao.ordenarPorValorAsc()
-                R.id.menu_ordenar_produtos_valor_desc -> produtoDao.ordenarPorValorDesc()
-                R.id.menu_ordenar_produtos_sem_ordem -> produtoDao.buscaTodos()
-                else -> null
-            }
-            produtosOrdenados?.let {
-                adapter.setData(it)
-            }
-            return super.onOptionsItemSelected(item)
+        produtosOrdenados?.let {
+            adapter.setData(it)
         }
+        return super.onOptionsItemSelected(item)
+    }
 }
 
 
